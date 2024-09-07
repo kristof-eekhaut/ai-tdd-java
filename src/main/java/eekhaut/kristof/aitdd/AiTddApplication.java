42ce0d9e05079e9beacde8c69449fe8a24dda0b2
kristof <kristofeekhaut@gmail.com>
2024-09-07 13:20:47 +0200

    Initial Spring Boot AI setup

---
+++ src/main/java/eekhaut/kristof/aitdd/AiTddApplication.java
@@ -0,0 +1,12 @@
+package eekhaut.kristof.aitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTddApplication.class, args);
+    }
+}
---
+++ src/test/java/eekhaut/kristof/aitdd/AiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package eekhaut.kristof.aitdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
