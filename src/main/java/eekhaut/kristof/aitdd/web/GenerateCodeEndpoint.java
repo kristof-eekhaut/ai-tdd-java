73d8a1f830249e5caefa17ad781e07e122e27bbb
kristof <kristofeekhaut@gmail.com>
2024-09-11 22:00:56 +0200

    Add java compiler

--- src/main/java/eekhaut/kristof/aitdd/web/GenerateCodeEndpoint.java
+++ src/main/java/eekhaut/kristof/aitdd/web/GenerateCodeEndpoint.java
@@ -1,5 +1,6 @@
 package eekhaut.kristof.aitdd.web;
 
+import eekhaut.kristof.aitdd.service.JavaCodeCompiler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -19,28 +21,29 @@ public class GenerateCodeEndpoint {
 
     private static final SystemPromptTemplate SYSTEM_PROMPT_TEMPLATE = new SystemPromptTemplate(
             """
-            You are an export Java programmer.
-            Your task is to create the code for a Java program that solves the problem described by the user.
-            
-            Respond only with the code. Do not provide additional explanation or text. Do not add formatting.
-            
-            The main class of your program has to be named "GeneratedCode".
-            The package name has to be "eekhaut.kristof.aitdd.generated".
-            Make sure to add all import statements at the top of the file.
-            All code has to be provided in the "GeneratedCode" class and has to be part of the same file.
-            
-            Below an example of a valid response is given within the <example> tags:
-            
-            <example>{example_code}</example>
-            """);
+                    You are an export Java programmer.
+                    Your task is to create the code for a Java program that solves the problem described by the user.
+                                
+                    Respond only with the code. Do not provide additional explanation or text. Do not add formatting.
+                                
+                    The main class of your program has to be named "GeneratedCode".
+                    The package name has to be "eekhaut.kristof.aitdd.generated".
+                    Make sure to add all import statements at the top of the file right below the package statement.
+                    All code has to be provided in the "GeneratedCode" class and has to be part of the same file.
+                    Don't write any tests. Only the production code.
+                                
+                    Below an example of a valid response is given within the <example> tags:
+                                
+                    <example>{example_code}</example>
+                    """);
 
     private static final String EXAMPLE_CODE = """
             package eekhaut.kristof.aitdd.generated;
-            
+                        
             import java.util.List;
-            
+                        
             public class GeneratedCode {
-            
+                        
                 public static Integer sum(List<Integer> numbers) {
                     return numbers.stream()
                             .reduce(Integer::sum)
@@ -51,10 +54,16 @@ public class GenerateCodeEndpoint {
 
     private static final PromptTemplate USER_PROMPT_TEMPLATE = new PromptTemplate("{query}");
 
-    private final ChatClient aiClient;
+    private final File GENERATED_CODE_DIR = new File("tmp/eekhaut/kristof/aitdd/generated");
+    private final String GENERATED_CODE_FILE_NAME = "GeneratedCode";
 
-    GenerateCodeEndpoint(ChatClient.Builder chatClientBuilder) {
+
+    private final ChatClient aiClient;
+    private final JavaCodeCompiler javaCodeCompiler;
+
+    GenerateCodeEndpoint(ChatClient.Builder chatClientBuilder, JavaCodeCompiler javaCodeCompiler) {
         this.aiClient = chatClientBuilder.build();
+        this.javaCodeCompiler = javaCodeCompiler;
     }
 
     @PostMapping("/generate-code")
@@ -69,6 +78,8 @@ public class GenerateCodeEndpoint {
         String output = response.content();
 
         log.info("Output:\n {}", output);
+
+        javaCodeCompiler.writeAndCompile(GENERATED_CODE_DIR, GENERATED_CODE_FILE_NAME, output);
 
         return ResponseEntity.ok(output);
     }
